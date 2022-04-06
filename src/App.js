adc5783760e6bb9c1a4724ac7537f3b5bd7be855
lillianaleecode <lilliana.lee@code.berlin>
2022-04-07 01:23:24 +0200

    progress connect database

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 
+import { useState } from 'react';
 import './App.css';
+import Axios from 'axios';
 
 function App() {
+
+  const [cakeName, setCakeName] = useState('')
+  const [review, setReview] = useState('')
+
+  const submitReview = () => {
+    Axios.post('http://localhost:3000/');
+
+  }
+
   return (
   
     <div className='App'>
@@ -9,12 +20,18 @@ function App() {
 
       <div className='form'>
         <label>Cake Name</label>
-        <input type="text" name="cakeName"/>
+        <input type="text" name="cakeName" onChange={(e)=> {
+          setCakeName(e.target.value)
 
-        <label>Cake Rating</label>
-        <input type="text" name="rating"/>
+        }}/>
 
-        <button>Submit</button>
+        <label>Cake Review:</label>
+        <input type="text" name="review" onChange={(e)=> {
+          setReview(e.target.value)
+
+        }}/>
+
+        <button onClick={submitReview}>Submit</button>
 
       </div>
 
@@ -26,3 +43,5 @@ function App() {
 }
 
 export default App;
+
+//npm start
